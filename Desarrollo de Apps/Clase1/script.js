749f839a985bdf62c5b66830af9a45496d232529
mariajo16ospi <maria_ospinapo@fet.edu.co>
2025-08-22 09:15:11 -0500

    Agregar a la lista

--- Desarrollo de Apps/Clase1/script.js
+++ Desarrollo de Apps/Clase1/script.js
@@ -1,5 +1,29 @@
 // console.log("hola")
 
-function cambiarTexto(){
-    document.getElementById("titulo").innerHTML = "Hola gente que programa mucho";
-}
+// Cambiar el titulo cuando se le presiona el boton
+// function cambiarTexto(){
+//     document.getElementById("titulo").innerHTML = "Hola gente que programa mucho";
+// }
+
+
+
+// Seleccionamos los elementos
+const agregarBtn = document.getElementById('agregarBtn');
+const Input = document.getElementById('Input');
+const lista = document.getElementById('lista');
+
+// Evento para el botón
+agregarBtn.addEventListener('click', function() {
+    const nuevaTareaTexto = Input.value.trim(); // quitamos espacios
+    if (nuevaTareaTexto === '') return; // si está vacío, no hace nada
+
+    // Crear el nuevo <li>
+    const nuevaTareaLi = document.createElement('li');
+    nuevaTareaLi.textContent = nuevaTareaTexto;
+
+    // Agregar el <li> a la lista
+    lista.appendChild(nuevaTareaLi);
+
+    // Limpiar el input
+    Input.value = '';
+});

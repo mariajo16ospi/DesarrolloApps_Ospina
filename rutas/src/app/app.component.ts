c609bedb67e920ce6c58e981d235dd2ab25bb9b7
mariajo16ospi <maria_ospinapo@fet.edu.co>
2025-09-05 11:26:21 -0500

    a

--- rutas/src/app/app.component.ts
+++ rutas/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Clase1Component } from "./clases/clase1/clase1.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Clase1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { Clase2Component } from './clases/clase2/clase2.component';
 import { Clase3Component } from './clases/clase3/clase3.component';
 
 export const routes: Routes = [
-  {path: 'clase1', component: Clase1Component},
-  {path: 'clase2', component: Clase2Component},
-  {path: 'clase3', component: Clase3Component}
+  // {path: 'clase1', component: Clase1Component},
+  // {path: 'clase2', component: Clase2Component},
+  // {path: 'clase3', component: Clase3Component}
 ];
+
+  

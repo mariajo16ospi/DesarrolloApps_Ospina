8e5e6add254a30becee1006834758c85d792c0db
mariajo16ospi <maria_ospinapo@fet.edu.co>
2025-09-05 09:17:27 -0500

    rutas

--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Clase1Component } from './clases/clase1/clase1.component';
+import { Clase2Component } from './clases/clase2/clase2.component';
+import { Clase3Component } from './clases/clase3/clase3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'clase1', component: Clase1Component},
+  {path: 'clase2', component: Clase2Component},
+  {path: 'clase3', component: Clase3Component}
+];

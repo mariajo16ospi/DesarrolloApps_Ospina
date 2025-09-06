b080bff17c641e386f42ef84b23dd0e7505de590
mariajo16ospi <maria_ospinapo@fet.edu.co>
2025-09-06 12:01:05 -0500

    Volvi a crear el proyecto

---
+++ ejerciciosCL/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Clase2Component } from './clases/clase2/clase2.component';
+export const routes: Routes = [
+  {
+    path: 'e2',
+    component: Clase2Component
+  }
+
+];
